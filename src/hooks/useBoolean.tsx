e501fac226cc6eb438bedb394697621895a13a41
Tiago Gonçalves <tiaguin180@gmail.com>
2022-01-01 21:48:51 -0300

    feat Exporting type

--- src/hooks/useBoolean.tsx
+++ src/hooks/useBoolean.tsx
@@ -1,5 +1,12 @@
 import { useState } from 'react'
 
+export type UseBooleanTypes = {
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+  state: boolean;
+  changeToTrue: () => void;
+  changeToFalse: () => void;
+}
+
 export function useBoolean(initialState?: boolean) {
   const [state, setState] = useState(initialState || false);
 
