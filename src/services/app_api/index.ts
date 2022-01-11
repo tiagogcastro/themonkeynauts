3392021ca1a08150654deea6c43e0ac6b3277e18
Tiago Gonçalves <tiaguin180@gmail.com>
2022-01-11 15:46:59 -0300

    fix: Changed import failed

--- src/services/app_api/index.ts
+++ src/services/app_api/index.ts
@@ -2,6 +2,6 @@ export * as user from './user';
 export * as ships from './ships';
 export * as monkeynauts from './monkeynauts';
 
-export * as UserType from './ships/types';
+export * as UserType from './user/types';
 export * as ShipType from './ships/types';
 export * as MonkeynautType from './monkeynauts/types';
