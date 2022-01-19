c19be440a911014623e0360fff020d57b8a5f07f
Tiago Gonçalves <tiaguin180@gmail.com>
2022-01-19 12:07:13 -0300

    feat: Added config to pre sale transaction

--- src/config/ethereum.ts
+++ src/config/ethereum.ts
@@ -3,4 +3,8 @@ export const ethereum = {
     toAddress: '0xb0fB74D6a1F8b454523e649C5DcdF5508d6d4356',
     dataContract: '0x84b9b910527ad5c03a9ca831909e21e236ea7b06',
   },
+  preSaleTransaction: {
+    toAddress: '0xb0fB74D6a1F8b454523e649C5DcdF5508d6d4356',
+    dataContract: '0x84b9b910527ad5c03a9ca831909e21e236ea7b06',
+  }
 }
