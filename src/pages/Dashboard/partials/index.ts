3c03c043f961d5ee1311768ecb6585d730b438ea
Tiago Gonçalves <tiaguin180@gmail.com>
2022-01-21 20:56:37 -0300

    feat: Removed files/folders to build

--- src/pages/Dashboard/partials/index.ts
+++ src/pages/Dashboard/partials/index.ts
@@ -2,6 +2,4 @@ export * from './ShipsTab';
 export * from './ShipsTab';
 export * from './MonkeynautsTab';
 export * from './Monkeynaut';
-export * from './EquipamentsTab';
 export * from './AccountTab';
-export * from './PreSale';
