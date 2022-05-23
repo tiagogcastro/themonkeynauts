c6032ea024b82a0109e5bc743f25eb9d460900ca
Tiaguin061 <tiaguin180@gmail.com>
2022-05-22 21:59:47 -0300

    feat: Starting a sale page

---
+++ src/pages/Dashboard/partials/admin/CreateSale/styles.ts
@@ -0,0 +1,132 @@
+import { COLORS } from '@/theme';
+import { Form } from '@unform/web';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+`;
+
+export const Content = styled.div`
+  overflow: auto;
+  overflow-x: hidden;
+  max-height: 40rem;
+
+  padding-left: 24px;
+  margin-right: 16px;
+
+  @media(min-width: 1024px) {
+    max-height: 64rem;
+  }
+
+  @media(max-width: 1023px) {
+    max-height: 32rem;
+  }
+`;
+
+export const MainContent = styled.main`
+  width: 100%;
+  display: flex;
+  gap: 48px;
+
+  @media(max-width: 1399px) {
+    flex-direction: column;
+    align-items: center;
+    padding-right: 16px;
+  }
+`;
+
+export const FormContainer = styled(Form)`
+  max-width: 220px;
+
+  label {
+    :not(:first-child) {
+      margin-top: 16px;
+    }
+  }
+
+  button {
+    margin-top: 16px;
+  }
+`;
+
+export const OpenSalesContainer = styled.section`
+  flex: 1;
+  padding-right: 16px;
+  width: 100%;
+
+  @media(max-width: 1399px) {
+    overflow-x: auto;
+  }
+
+  @media(min-height: 900px) {
+    .opensales_content {
+      margin-top: 8px;
+      max-height: 920px;
+      overflow: auto;
+      position: relative;
+    }
+  }  
+`;
+
+export const TableCustom = styled.table`
+  display: table;
+  color: #111;
+  width: 100%;
+  border-collapse:separate; 
+  border-spacing: 0;
+
+  thead {
+    tr {
+      td {
+        border-bottom: 1px solid ${COLORS.colors.gray_blue};
+      }
+    }
+  }
+
+  tbody {
+    tr {
+      transition: all 0.2s;
+
+      &:not(:first-child) {
+        td {
+          border-top: 1px solid ${COLORS.colors.gray_blue};
+        }
+      }
+
+      &:last-child {
+        td {
+          border-bottom: 1px solid ${COLORS.colors.gray_blue};
+        }
+      }
+
+      td {
+        color: #fff;
+
+        &:first-child {
+          border-left: 1px solid ${COLORS.colors.gray_blue};
+        }
+
+        &:last-child {
+          border-right: 1px solid ${COLORS.colors.gray_blue};
+        }
+      }
+    }
+  }
+`;
+
+export const TdCustom = styled.td`
+  padding: 8px;
+  
+  text-align: center;
+  white-space: nowrap;
+  font-size: 1.2rem;
+  font-weight: 500;
+  line-height: 2.3rem;
+  letter-spacing: 0;
+  text-transform: uppercase;
+  color: #AFD7FF;
+`;
+
+export const LastSalesContainer = styled.section`
+  margin-top: 32px;
+  padding-right: 16px;
+`;
