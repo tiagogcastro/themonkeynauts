7820379da62f8005294a673164fc0e9b832fb8ae
Tiaguin061 <tiaguin180@gmail.com>
2022-05-28 21:27:26 -0300

    feat: Changed sessions to auth name

---
+++ src/modules/players/infra/http/routes/auth.routes.ts
@@ -0,0 +1,19 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+
+import { appPlayerAuthController } from '../controllers/app-player-auth';
+
+const authRouter = Router();
+
+authRouter.post(
+  '/app',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  (request, response) => appPlayerAuthController.handle(request, response),
+);
+
+export { authRouter };
