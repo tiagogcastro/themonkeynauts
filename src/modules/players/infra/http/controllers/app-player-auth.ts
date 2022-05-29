e9fc9aeb47ce4957e997b57fa1e1ae37ce9c7454
Tiaguin061 <tiaguin180@gmail.com>
2022-05-28 21:29:53 -0300

    feat: Created controller to player auth

---
+++ src/modules/players/infra/http/controllers/app-player-auth.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { AppPlayerAuthBusinessLogic } from '@modules/players/core/business-logic/app-auth-player';
+
+import { instanceToInstance } from '@shared/infra/helpers/instance-to-instance';
+
+class AppPlayerAuthController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+    console.log(request.ip);
+
+    const appPlayerAuthBusinessLogic = container.resolve(
+      AppPlayerAuthBusinessLogic,
+    );
+
+    const { player, token } = await appPlayerAuthBusinessLogic.execute({
+      email,
+      password,
+    });
+
+    return response.status(200).json({
+      player: instanceToInstance('player', player),
+      token,
+    });
+  }
+}
+
+const appPlayerAuthController = new AppPlayerAuthController();
+
+export { appPlayerAuthController };
