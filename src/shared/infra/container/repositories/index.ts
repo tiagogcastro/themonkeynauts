855e1c0588236c1888c67c5199390c27cf1cab66
Tiaguin061 <tiaguin180@gmail.com>
2022-05-28 21:28:56 -0300

    feat: Register app player auth repository in container

--- src/shared/infra/container/repositories/index.ts
+++ src/shared/infra/container/repositories/index.ts
@@ -8,11 +8,14 @@ import { PrismaResourcesRepository } from '@modules/players/infra/database/prism
 import { IResourcesRepository } from '@modules/players/domain/repositories/resources-repository';
 import { ILogsRepository } from '@modules/logs/domain/repositories/logs-repositories';
 import { PrismaLogsRepository } from '@modules/logs/infra/database/prisma/repositories/prisma-logs-repositories';
+import { IAppPlayerAuthRepository } from '@modules/players/domain/repositories/app-player-auth-repository';
+import { PrismaAppPlayerAuthRepository } from '@modules/players/infra/database/prisma/repositories/prisma-app-player-auth-repository';
 
 container.registerSingleton<IPlayersRepository>(
   'PlayersRepository',
   PrismaPlayersRepository,
 );
+
 container.registerSingleton<IResourcesRepository>(
   'ResourcesRepository',
   PrismaResourcesRepository,
@@ -22,7 +25,13 @@ container.registerSingleton<IPlayerTokensRepository>(
   'PlayerTokensRepository',
   PrismaPlayerTokensRepository,
 );
+
 container.registerSingleton<ILogsRepository>(
   'LogsRepository',
   PrismaLogsRepository,
+);
+
+container.registerSingleton<IAppPlayerAuthRepository>(
+  'AppPlayerAuthRepository',
+  PrismaAppPlayerAuthRepository,
 );
