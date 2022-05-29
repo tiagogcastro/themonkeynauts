a4d47f614ba1d54520d86038a2bfbd6fdcc7dd99
Tiaguin061 <tiaguin180@gmail.com>
2022-05-28 21:27:55 -0300

    feat: Changed return in jwt token provider

--- src/shared/infra/providers/jwt-token-provider.ts
+++ src/shared/infra/providers/jwt-token-provider.ts
@@ -1,20 +1,25 @@
+import { JwtPayload, sign, verify } from 'jsonwebtoken';
+
 import { Player } from '@modules/players/domain/entities/player';
+import { PlayerAuth } from '@modules/players/domain/entities/player-auth';
+
 import { ITokenProvider } from '@shared/domain/providers/token-provider';
-import { sign, verify } from 'jsonwebtoken';
+
 import { authConfig } from '../../../config/auth';
 
 export class JWTokenProvider implements ITokenProvider {
-  verify(token: string): void {
+  verify<T = JwtPayload>(token: string): T {
     const { secret } = authConfig;
 
-    verify(token, secret);
+    return verify(token, secret) as T;
   }
 
-  generate(player: Player): string {
+  generate(playerAuth: PlayerAuth, player?: Player): string {
     const { secret, expiresIn } = authConfig;
 
-    const token = sign({}, secret, {
-      subject: player.id,
+    const token = sign({
+      ...playerAuth
+    }, secret, {
       expiresIn,
     });
 
