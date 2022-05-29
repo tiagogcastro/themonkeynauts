b83660b37648a82157cd04a25d5a62f8ba0c53d0
Tiaguin061 <tiaguin180@gmail.com>
2022-05-28 21:28:09 -0300

    feat: Compare date type

--- src/shared/infra/providers/datefns-date-provider.ts
+++ src/shared/infra/providers/datefns-date-provider.ts
@@ -1,8 +1,16 @@
 import { IDateProvider } from '@shared/domain/providers/date-provider';
-import { addHours, isAfter } from 'date-fns';
+import { addHours, isAfter, parseISO } from 'date-fns';
 
 class DateFnsDateProvider implements IDateProvider {
   isAfter(date: Date, dateToCompare: Date): boolean {
+    if(typeof date === 'string') {
+      date = parseISO(date);
+    }
+
+    if(typeof dateToCompare === 'string') {
+      dateToCompare = parseISO(dateToCompare);
+    }
+
     return isAfter(date, dateToCompare)
   }
   
