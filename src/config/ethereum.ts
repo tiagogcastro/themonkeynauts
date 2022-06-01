b3a4080a2ea4a9f9f6612d9172197d978fd9e7e7
pprograminha <marcosproenca144@gmail.com>
2022-06-01 16:55:12 -0300

    refactor: new pre sale to adress

--- src/config/ethereum.ts
+++ src/config/ethereum.ts
@@ -4,7 +4,7 @@ export const ethereum = {
     dataContract: '0x84b9b910527ad5c03a9ca831909e21e236ea7b06',
   },
   preSaleTransaction: {
-    toAddress: '0xb0fB74D6a1F8b454523e649C5DcdF5508d6d4356',
+    toAddress: '0x11b05c7ec22016ebcdb5ED4CDa614b73507B2aa3',
     dataContract: '0x84b9b910527ad5c03a9ca831909e21e236ea7b06',
   }
 }
