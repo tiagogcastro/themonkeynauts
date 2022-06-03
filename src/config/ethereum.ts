409ef2e425c8affe7c391faafb3e30bd8d0f2b71
Tiaguin061 <tiaguin180@gmail.com>
2022-06-03 15:22:55 -0300

    improve: Changed private sale data contract

--- src/config/ethereum.ts
+++ src/config/ethereum.ts
@@ -5,6 +5,6 @@ export const ethereum = {
   },
   privateSaleTransaction: {
     toAddress: '0x6c4543b80c4A2Bb922e31E045a67217F13caf797',
-    dataContract: '0x84b9b910527ad5c03a9ca831909e21e236ea7b06',
+    dataContract: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
   }
 }
