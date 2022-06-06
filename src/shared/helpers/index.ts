a637398db2eb0979e1dd234b7caaad945ed02eb0
Tiaguin061 <tiaguin180@gmail.com>
2022-06-06 11:44:45 -0300

    feat: Exported helpers

---
+++ src/shared/helpers/index.ts
@@ -0,0 +1,7 @@
+export * from './commons';
+export * from './debounce';
+export * from './generate-space-name';
+export * from './get-percentage-int';
+export * from './get-random-int';
+export * from './rarity';
+export * from './retry';
