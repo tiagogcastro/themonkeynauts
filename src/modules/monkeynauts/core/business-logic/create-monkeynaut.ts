70a8605d4b377655dd5b7ea0bab56e25bfffbef1
Tiaguin061 <tiaguin180@gmail.com>
2022-06-06 11:45:41 -0300

    feat: Created logic to generate rank and attributes

--- src/modules/monkeynauts/core/business-logic/create-monkeynaut.ts
+++ src/modules/monkeynauts/core/business-logic/create-monkeynaut.ts
@@ -1,10 +1,14 @@
+import { inject, injectable } from 'tsyringe';
+
+import { getRandomInt, getPercentageInt, rarity } from '@shared/helpers';
+
 import {
   IMonkeynaut,
   Monkeynaut,
 } from '@modules/monkeynauts/domain/entities/monkeynaut';
+
 import { CreateMonkeynautRequestDTO } from '@modules/monkeynauts/dtos/create-monkeynaut-request';
-import { getRandomInt } from '@shared/helpers/get-random-int';
-import { inject, injectable } from 'tsyringe';
+
 import { IMonkeynautsRepository } from '../../domain/repositories/monkeynauts-repositories';
 
 @injectable()
@@ -17,22 +21,69 @@ class CreateMonkeynautBusinessLogic {
   async execute({
     bonus,
     bonus_value,
+
     breed_count,
+
     class: _class,
-    rank,
+    rank: _rank,
+
     energy,
-    health,
     max_energy,
+
+    health: _health,
+    speed: _speed,
+    power: _power,
+    resistence: _resistence,
+
     player_id,
   }: CreateMonkeynautRequestDTO): Promise<IMonkeynaut> {
+    const RANKS = {
+      PRIVATE: { percentage: 0 },
+      SERGEANT: { percentage: 15 },
+      CAPTAIN: { percentage: 30 },
+      MAJOR: { percentage: 45 },
+    };
+
     const baseHealth = getRandomInt(250, 350);
     const baseSpeed = getRandomInt(20, 50);
     const basePower = getRandomInt(20, 50);
     const baseResistence = getRandomInt(20, 50);
 
-    // const rank = getRandomInt(20, 50);
+    const classe = await rarity({
+      soldier: 40,
+      enginner: 30,
+      scientist: 30,
+    });
 
-    const { monkeynaut, assign } = new Monkeynaut({
+    const rank = await rarity({
+      private: 50,
+      sergeant: 30,
+      captain: 15,
+      major: 5,
+    });
+
+    const { percentage } = RANKS[rank];
+
+    const attributesFinalValues = {
+      health: getPercentageInt({
+        percentage,
+        value: baseHealth,
+      }),
+      speed: getPercentageInt({
+        percentage,
+        value: baseSpeed,
+      }),
+      power: getPercentageInt({
+        percentage,
+        value: basePower,
+      }),
+      resistence: getPercentageInt({
+        percentage,
+        value: baseResistence,
+      }),
+    };
+
+    const { monkeynaut } = new Monkeynaut({
       avatar: null,
 
       baseHealth,
@@ -40,17 +91,14 @@ class CreateMonkeynautBusinessLogic {
       basePower,
       baseResistence,
 
-      power: 1,
-      resistence: 1,
-      speed: 1,
-      health,
+      ...attributesFinalValues,
 
       bonus,
       bonusValue: bonus_value,
       breedCount: breed_count,
 
-      class: _class,
-      rank,
+      class: _class || classe,
+      rank: _rank || rank,
 
       energy,
       maxEnergy: max_energy,
