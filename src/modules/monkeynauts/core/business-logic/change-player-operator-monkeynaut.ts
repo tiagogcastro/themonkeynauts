b5ff887eecedcce6166352a4da743297fdc3c59b
Tiaguin061 <tiaguin180@gmail.com>
2022-06-07 17:25:07 -0300

    feat: Created method to change monkeynaut player operator

---
+++ src/modules/monkeynauts/core/business-logic/change-player-operator-monkeynaut.ts
@@ -0,0 +1,71 @@
+import { inject, injectable } from 'tsyringe';
+
+import {
+  IMonkeynaut,
+  Monkeynaut,
+} from '@modules/monkeynauts/domain/entities/monkeynaut';
+
+import { ChangePlayerOperatorMonkeynautRequestDTO } from '@modules/monkeynauts/dtos/change-player-operator-monkeynaut-request';
+
+import { IPlayersRepository } from '@modules/players/domain/repositories/players-repository';
+import { AppError } from '@shared/errors/app-error';
+import { IMonkeynautsRepository } from '../../domain/repositories/monkeynauts-repositories';
+
+@injectable()
+class ChangePlayerOperatorMonkeynautBusinessLogic {
+  constructor(
+    @inject('MonkeynautsRepository')
+    private monkeynautsRepository: IMonkeynautsRepository,
+
+    @inject('PlayersRepository')
+    private playerRepository: IPlayersRepository,
+  ) {}
+
+  async execute({
+    actual_operator_player_id,
+    new_operator_player_id,
+    monkeynaut_id,
+  }: ChangePlayerOperatorMonkeynautRequestDTO): Promise<IMonkeynaut> {
+    const foundActualOperatorPlayer = await this.playerRepository.findById(
+      actual_operator_player_id,
+    );
+
+    if (!foundActualOperatorPlayer) {
+      throw new AppError('Actual operator player does not exist', 404);
+    }
+
+    const foundNewOperatorPlayer = await this.playerRepository.findById(
+      new_operator_player_id,
+    );
+
+    if (!foundNewOperatorPlayer) {
+      throw new AppError('New player operator does not exist', 404);
+    }
+
+    const foundMonkeynaut = await this.monkeynautsRepository.findById(
+      monkeynaut_id,
+    );
+
+    if (!foundMonkeynaut) {
+      throw new AppError('Actual operator player does not exist', 404);
+    }
+
+    const { monkeynaut } = new Monkeynaut(
+      {
+        ...foundMonkeynaut,
+        playerId: foundNewOperatorPlayer.id,
+      },
+      {
+        id: foundMonkeynaut.id,
+        createdAt: foundMonkeynaut.createdAt,
+        updatedAt: new Date(),
+      },
+    );
+
+    await this.monkeynautsRepository.save(monkeynaut);
+
+    return monkeynaut;
+  }
+}
+
+export { ChangePlayerOperatorMonkeynautBusinessLogic };
--- src/modules/monkeynauts/infra/http/routes/monkeynauts.routes.ts
+++ src/modules/monkeynauts/infra/http/routes/monkeynauts.routes.ts
@@ -1,9 +1,12 @@
-import ensureAdministrator from '@modules/players/infra/http/middlewares/ensure-administrator';
-import { ensureAuthenticated } from '@modules/players/infra/http/middlewares/ensure-authenticated';
 import { celebrate, Joi, Segments } from 'celebrate';
 import { Router } from 'express';
+
+import ensureAdministrator from '@modules/players/infra/http/middlewares/ensure-administrator';
+import { ensureAuthenticated } from '@modules/players/infra/http/middlewares/ensure-authenticated';
+
 import { createMonkeynautController } from '../controllers/create-monkeynaut';
 import { listMonkeynautsController } from '../controllers/list-monkeynauts';
+import { changePlayerOperatorMonkeynautController } from '../controllers/change-player-operator-monkeynaut';
 
 const monkeynautsRouter = Router();
 
@@ -24,23 +27,44 @@ monkeynautsRouter.post(
   ensureAuthenticated,
   celebrate({
     [Segments.BODY]: {
-      player_id: Joi.string().uuid().required(),
+      player_id: Joi.string().uuid(),
 
-      bonus: Joi.string().required(),
-      bonus_value: Joi.number().required(),
+      bonus_description: Joi.string(),
+      bonus_value: Joi.number(),
 
-      breed_count: Joi.number().required(),
+      base_attributes: Joi.object({
+        base_health: Joi.number().min(250).max(350),
+        base_speed: Joi.number().min(20).max(50),
+        base_power: Joi.number().min(20).max(50),
+        base_resistence: Joi.number().min(20).max(50),
+      }),
+
+      breed_count: Joi.number(),
 
       class: Joi.string().regex(/^(SOLDIER|ENGINEER|SCIENTIST)$/),
       rank: Joi.string().regex(/^(PRIVATE|SERGEANT|CAPTAIN|MAJOR)$/),
 
-      energy: Joi.number().required(),
-      max_energy: Joi.number().required(),
+      energy: Joi.number(),
+      max_energy: Joi.number(),
 
       name: Joi.string(),
     },
   }),
   (request, response) => createMonkeynautController.handle(request, response),
+);
+
+monkeynautsRouter.put(
+  '/change-player-operator',
+  ensureAuthenticated,
+  celebrate({
+    [Segments.BODY]: {
+      actual_operator_player_id: Joi.string().uuid(),
+      new_operator_player_id: Joi.string().uuid(),
+      monkeynaut_id: Joi.string().uuid().required(),
+    },
+  }),
+  (request, response) =>
+    changePlayerOperatorMonkeynautController.handle(request, response),
 );
 
 export { monkeynautsRouter };
