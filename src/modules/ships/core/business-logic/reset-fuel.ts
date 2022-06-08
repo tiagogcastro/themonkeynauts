ab9a1ff12b5b457cc1d80b7ecdf765804e59ea26
pprograminha <marcosproenca144@gmail.com>
2022-06-08 11:40:26 -0300

    feat: reset fuel

---
+++ src/modules/ships/core/business-logic/reset-fuel.ts
@@ -0,0 +1,27 @@
+import { ICronJobProvider } from '@shared/domain/providers/cronjob-provider';
+import { inject, injectable } from 'tsyringe';
+import { IShipsRepository } from '../../domain/repositories/ships-repositories';
+
+@injectable()
+class ResetFuelBusinessLogic {
+  constructor(
+    @inject('ShipsRepository')
+    private shipsRepository: IShipsRepository,
+
+    @inject('CronJobProvider')
+    private cronJobProvider: ICronJobProvider,
+  ) {}
+
+  async execute(): Promise<void> {
+    await this.cronJobProvider.run({
+      cronTime: '0 0 * * *',
+      onTick: async () => {
+        await this.shipsRepository.saveMany({
+          canRefuelAtStation: true,
+        });
+      },
+    });
+  }
+}
+
+export { ResetFuelBusinessLogic };
