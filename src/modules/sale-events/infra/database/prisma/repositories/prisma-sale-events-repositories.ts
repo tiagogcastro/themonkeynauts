53784a3f0ec9b5b3cea35eaa981238a18f684ede
Tiaguin061 <tiaguin180@gmail.com>
2022-06-10 23:09:12 -0300

    feat: Created methods to repository

--- src/modules/sale-events/infra/database/prisma/repositories/prisma-sale-events-repositories.ts
+++ src/modules/sale-events/infra/database/prisma/repositories/prisma-sale-events-repositories.ts
@@ -1,7 +1,10 @@
+import { IMonkeynautSale } from '@modules/sale-events/domain/entities/monkeynaut-sale';
+import { IPackSale } from '@modules/sale-events/domain/entities/pack-sale';
 import {
   ISaleEvent,
   SaleEvent,
 } from '@modules/sale-events/domain/entities/sale-event';
+import { IShipSale } from '@modules/sale-events/domain/entities/ship-sale';
 import { ISaleEventsRepository } from '@modules/sale-events/domain/repositories/sale-events-repositories';
 import { SaleEvent as PrismaSaleEvent } from '@prisma/client';
 import { prisma } from '@shared/infra/database/prisma/client';
@@ -15,7 +18,40 @@ const parseSaleEvent = (saleEvent: PrismaSaleEvent): ISaleEvent => {
 };
 
 class PrismaSaleEventsRepository implements ISaleEventsRepository {
-  async create({ id: saleEventId, ...props }: ISaleEvent): Promise<void> {
+  async createMonkeynautSale({
+    id: monkeynautId,
+    ...props
+  }: IMonkeynautSale): Promise<void> {
+    await prisma.monkeynautSale.create({
+      data: {
+        id: monkeynautId,
+        ...props,
+      },
+    });
+  }
+
+  async createShipSale({ id: shipId, ...props }: IShipSale): Promise<void> {
+    await prisma.shipSale.create({
+      data: {
+        id: shipId,
+        ...props,
+      },
+    });
+  }
+
+  async createPackSale({ id: packId, ...props }: IPackSale): Promise<void> {
+    await prisma.packSale.create({
+      data: {
+        id: packId,
+        ...props,
+      },
+    });
+  }
+
+  async createSaleEvent({
+    id: saleEventId,
+    ...props
+  }: ISaleEvent): Promise<void> {
     await prisma.saleEvent.create({
       data: {
         id: saleEventId,
