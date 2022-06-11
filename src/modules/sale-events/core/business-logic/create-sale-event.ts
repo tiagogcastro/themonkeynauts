8ae0c50c6f96a13ba674db1c6184327ff6f862fd
Tiaguin061 <tiaguin180@gmail.com>
2022-06-10 23:06:44 -0300

    feat: Created logic to save sale event

--- src/modules/sale-events/core/business-logic/create-sale-event.ts
+++ src/modules/sale-events/core/business-logic/create-sale-event.ts
@@ -1,26 +1,32 @@
-/* eslint-disable max-classes-per-file */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable no-case-declarations */
 import { inject, injectable } from 'tsyringe';
-
-import {
-  ISaleEvent,
-  SaleEvent,
-} from '@modules/sale-events/domain/entities/sale-event';
 
 import { ISaleEventsRepository } from '@modules/sale-events/domain/repositories/sale-events-repositories';
 import { CreateSaleEventRequestDTO } from '@modules/sale-events/dtos/create-sale-event-request';
-import { SaleEventType } from '@modules/sale-events/domain/enums/sale-event-type';
 
 import { IDateProvider } from '@shared/domain/providers/date-provider';
 import { AppError } from '@shared/errors/app-error';
 
-import { MonkeynautSale } from '@modules/sale-events/domain/entities/monkeynaut-sale';
-import { PackSale } from '@modules/sale-events/domain/entities/pack-sale';
-import { ShipSale } from '@modules/sale-events/domain/entities/ship-sale';
-import { PrismaSaleEventsRepository } from '@modules/sale-events/infra/database/prisma/repositories/prisma-sale-events-repositories';
-import { DateFnsDateProvider } from '@shared/infra/providers/datefns-date-provider';
+import {
+  MonkeynautSale,
+  IMonkeynautSale,
+} from '@modules/sale-events/domain/entities/monkeynaut-sale';
+import {
+  PackSale,
+  IPackSale,
+} from '@modules/sale-events/domain/entities/pack-sale';
+import {
+  ShipSale,
+  IShipSale,
+} from '@modules/sale-events/domain/entities/ship-sale';
+import {
+  SaleEvent,
+  ISaleEvent,
+} from '@modules/sale-events/domain/entities/sale-event';
+
+type Response = {
+  saleEvent: ISaleEvent;
+  saleData: IPackSale | IShipSale | IMonkeynautSale;
+};
 
 @injectable()
 class CreateSaleEventBusinessLogic {
@@ -35,16 +41,23 @@ class CreateSaleEventBusinessLogic {
   ) {}
 
   async execute({
-    sale,
+    salePack,
+    saleShip,
+    saleMonkeynaut,
     type,
     endDate,
     startDate,
-  }: CreateSaleEventRequestDTO): Promise<ISaleEvent> {
+    ...data
+  }: CreateSaleEventRequestDTO): Promise<Response> {
     const currentDate = new Date();
 
     if (endDate) {
       if (this.dateProvider.isBefore(endDate, currentDate)) {
         throw new AppError('End date must be after current date');
+      }
+
+      if (this.dateProvider.isBefore(startDate, endDate)) {
+        throw new AppError('The start date must be less than the end time');
       }
     }
 
@@ -54,23 +67,74 @@ class CreateSaleEventBusinessLogic {
 
     const { saleEvent } = new SaleEvent({
       active: true,
+      type,
     });
 
-    // switch (type) {
-    //   case 'MONKEYNAUT':
-    //     const monkeynautSale = sale as MonkeynautSaleUniqueProps;
+    await this.saleEventsRepository.createSaleEvent(saleEvent);
 
-    //     if(monkeynautSale.)
-    //     break;
-    //   case 'SHIP':
-    //     break;
-    //   case 'PACK':
-    //     break;
+    const createSaleCommons = {
+      endDate,
+      startDate,
+      saleEventId: saleEvent.id,
+      ...data,
+    };
 
-    //   default:
-    //     break;
-    // }
+    const saleData = {
+      saleId: '',
+      dataByType: {} as IPackSale | IShipSale | IMonkeynautSale,
+    };
 
-    return saleEvent;
+    switch (type) {
+      case 'MONKEYNAUT':
+        if (saleMonkeynaut) {
+          const { monkeynautSale } = new MonkeynautSale({
+            ...saleMonkeynaut,
+            ...createSaleCommons,
+          });
+
+          saleData.saleId = monkeynautSale.id;
+          saleData.dataByType = monkeynautSale;
+
+          await this.saleEventsRepository.createMonkeynautSale(monkeynautSale);
+        }
+
+        break;
+      case 'SHIP':
+        if (saleShip) {
+          const { shipSale } = new ShipSale({
+            ...saleShip,
+            ...createSaleCommons,
+          });
+
+          saleData.saleId = shipSale.id;
+          saleData.dataByType = shipSale;
+
+          await this.saleEventsRepository.createShipSale(shipSale);
+        }
+        break;
+      case 'PACK':
+        if (salePack) {
+          const { packSale } = new PackSale({
+            ...salePack,
+            ...createSaleCommons,
+          });
+
+          saleData.saleId = packSale.id;
+          saleData.dataByType = packSale;
+
+          await this.saleEventsRepository.createPackSale(packSale);
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    return {
+      saleEvent,
+      saleData: saleData.dataByType,
+    };
   }
 }
+
+export { CreateSaleEventBusinessLogic };
