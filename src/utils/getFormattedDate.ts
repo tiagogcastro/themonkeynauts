ac6a1eef5a014d2cf12589e262a64d4190f3565e
Tiaguin061 <tiaguin180@gmail.com>
2022-06-16 18:05:25 -0300

    refactor: Removed hour

--- src/utils/getFormattedDate.ts
+++ src/utils/getFormattedDate.ts
@@ -5,11 +5,9 @@ export function addZeroBeforeDate(date: number) {
 export function getFormattedDate(date: Date | string) {
   const formatToDate = new Date(date);
 
-  const minutes = addZeroBeforeDate(formatToDate.getMinutes());
-  const hour = addZeroBeforeDate(formatToDate.getHours());
   const day = addZeroBeforeDate(formatToDate.getDate());
   const month = addZeroBeforeDate(formatToDate.getMonth());
   const fullYear = formatToDate.getFullYear();
 
-  return `Day ${day}:${month}:${fullYear} | ${hour}:${minutes}`;
+  return `Day ${day}:${month}:${fullYear}`;
 }
