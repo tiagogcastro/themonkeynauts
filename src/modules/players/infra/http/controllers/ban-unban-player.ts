4802eedc5efb3388fe6b76262864aa3f1dcb20c8
pprograminha <marcosproenca144@gmail.com>
2022-06-23 14:08:22 -0300

    chore: ban unban player controller

---
+++ src/modules/players/infra/http/controllers/ban-unban-player.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { IController } from '@shared/core/infra/controller';
+import {
+  clientError,
+  fail,
+  HttpResponse,
+  ok,
+} from '@shared/core/infra/http-response';
+import {
+  BanUnbanPlayerBusinessLogic,
+  BanUnbanPlayerRequestDTO,
+} from '@modules/players/core/business-logic/ban-unban-player';
+import { instanceToInstance } from '@shared/helpers/instance-to-instance';
+
+class BanUnbanPlayerController
+  implements IController<BanUnbanPlayerRequestDTO>
+{
+  async handle({ playerId }: BanUnbanPlayerRequestDTO): Promise<HttpResponse> {
+    try {
+      const banUnbanPlayerBusinessLogic = container.resolve(
+        BanUnbanPlayerBusinessLogic,
+      );
+
+      const result = await banUnbanPlayerBusinessLogic.execute({
+        playerId,
+      });
+
+      if (result.isLeft()) {
+        const error = result.value;
+
+        return clientError(error);
+      }
+
+      return ok(instanceToInstance('player', result.value));
+    } catch (error) {
+      return fail(error as Error);
+    }
+  }
+}
+
+const banUnbanPlayerController = new BanUnbanPlayerController();
+
+export { banUnbanPlayerController };
