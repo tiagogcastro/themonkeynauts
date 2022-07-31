5c6ada18830a8b2f22d3d6916849c60647539d14
Tiaguin061 <tiaguin180@gmail.com>
2022-07-31 15:33:14 -0300

    feat: Using spc code

--- src/pages/Dashboard/partials/AccountTab/index.tsx
+++ src/pages/Dashboard/partials/AccountTab/index.tsx
@@ -5,12 +5,12 @@ import { FiLogOut } from 'react-icons/fi';
 import { toast } from 'react-toastify';
 
 import { useAuth, useBoolean } from '@/hooks';
-// import { paymentByEthereum } from '@/utils';
+import { paymentByEthereum } from '@/utils';
 
 import { Wallet } from '@/components/modals/Wallet';
 import { Button } from '@/components';
 
-// import { ethereum as ethereumConfig } from '@/config/ethereum';
+import { ethereum as ethereumConfig } from '@/config/ethereum';
 
 import bronze_ore from '@/assets/images/bronze_ore.png';
 import iron_ore from '@/assets/images/iron_ore.png';
@@ -33,11 +33,13 @@ import {
   Resources,
   Resource,
 
-  // Spc,
+  Spc,
 } from './styles';
-// import { ApiError } from '@/utils/apiError';
-// import { baseApi } from '@/services/api';
-// import { verifyWallet } from '@/utils/wallet';
+import { ApiError } from '@/utils/apiError';
+import { baseApi } from '@/services/api';
+import { verifyWallet } from '@/utils/wallet';
+import { useState } from 'react';
+import { ethers } from 'ethers';
 
 export type HandleChange = {
   event: React.ChangeEvent<HTMLInputElement>;
@@ -45,221 +47,224 @@ export type HandleChange = {
 
 export function AccountTab() {
   const { player, signOut } = useAuth();
+
+  const withdrawDepositPermission = ['admin', 'owner'];
+  const withdrawDepositShow = player && withdrawDepositPermission.includes(player?.player.role.toLowerCase());
   
   const walletModalIsOpen = useBoolean();
-  // const depositButtonHasBlocked = useBoolean(false);
-  // const withdrawButtonHasBlocked = useBoolean(false);
+  const depositButtonHasBlocked = useBoolean(false);
+  const withdrawButtonHasBlocked = useBoolean(false);
 
-  // const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
 
-  // function handleChangeInput({
-  //   event,
-  // }: HandleChange) {
-  //   let value: string | number = event.target.value.replace(/[^0-9.]/g, '');
+  function handleChangeInput({
+    event,
+  }: HandleChange) {
+    let value: string | number = event.target.value.replace(/[^0-9.]/g, '');
 
-  //   setInputValue(String(value));
-  // }
+    setInputValue(String(value));
+  }
 
-  // function verifyIfHasInputValue(value: any, message: string) {
-  //   if(!value) {
-  //     toast(message, {
-  //       autoClose: 7000,
-  //       pauseOnHover: true,
-  //       type: 'warning',
-  //       style: {
-  //         background: COLORS.global.white_0,
-  //         color: COLORS.global.black_0 ,
-  //         fontSize: 14,
-  //         fontFamily: 'Orbitron, sans-serif',
-  //       }
-  //     });
+  function verifyIfHasInputValue(value: any, message: string) {
+    if(!value) {
+      toast(message, {
+        autoClose: 7000,
+        pauseOnHover: true,
+        type: 'warning',
+        style: {
+          background: COLORS.global.white_0,
+          color: COLORS.global.black_0 ,
+          fontSize: 14,
+          fontFamily: 'Orbitron, sans-serif',
+        }
+      });
 
-  //     return false;
-  //   }
-  //   return true;
-  // }
+      return false;
+    }
+    return true;
+  }
 
-  // function commonToast() {
-  //   toast(`${player?.player.nickname}, please wait for the metamask window to open.`, {
-  //     autoClose: 5000,
-  //     pauseOnHover: true,
-  //     type: 'info',
-  //     style: {
-  //       background: COLORS.global.white_0,
-  //       color: COLORS.global.black_0,
-  //       fontSize: 14,
-  //       fontFamily: 'Orbitron, sans-serif',
-  //     }
-  //   });
+  function commonToast() {
+    toast(`${player?.player.nickname}, please wait for the metamask window to open.`, {
+      autoClose: 5000,
+      pauseOnHover: true,
+      type: 'info',
+      style: {
+        background: COLORS.global.white_0,
+        color: COLORS.global.black_0,
+        fontSize: 14,
+        fontFamily: 'Orbitron, sans-serif',
+      }
+    });
 
-  //   toast(`if it doesn't open a popup, check your metamask`, {
-  //     autoClose: 6000,
-  //     pauseOnHover: true,
-  //     type: 'info',
-  //     style: {
-  //       background: COLORS.global.white_0,
-  //       color: COLORS.global.black_0,
-  //       fontSize: 14,
-  //       fontFamily: 'Orbitron, sans-serif',
-  //     }
-  //   });
-  // }
+    toast(`if it doesn't open a popup, check your metamask`, {
+      autoClose: 6000,
+      pauseOnHover: true,
+      type: 'info',
+      style: {
+        background: COLORS.global.white_0,
+        color: COLORS.global.black_0,
+        fontSize: 14,
+        fontFamily: 'Orbitron, sans-serif',
+      }
+    });
+  }
 
-  // async function handleSubmitWithdraw(event: React.FormEvent<HTMLButtonElement>) {
-  //   event.preventDefault();
+  async function handleSubmitWithdraw(event: React.FormEvent<HTMLButtonElement>) {
+    event.preventDefault();
 
-  //   const verifyHasInputValue = verifyIfHasInputValue(inputValue, 'You need to enter the number to continue the withdraw.');
-  //   if(verifyHasInputValue) {
-  //     commonToast();
+    const verifyHasInputValue = verifyIfHasInputValue(inputValue, 'You need to enter the number to continue the withdraw.');
+    if(verifyHasInputValue) {
+      commonToast();
 
-  //     withdrawButtonHasBlocked.changeToTrue();
+      withdrawButtonHasBlocked.changeToTrue();
 
-  //     try {
-  //       await baseApi.post('/players/withdraw-tokens', {
-  //         amount: inputValue
-  //       });
+      try {
+        await baseApi.post('/players/withdraw-tokens', {
+          amount: inputValue
+        });
         
-  //       toast(`${player?.player.nickname}, your ${inputValue} withdraw was a success`, {
-  //         autoClose: 5000,
-  //         pauseOnHover: true,
-  //         type: 'success',
-  //         style: {
-  //           background: COLORS.global.white_0,
-  //           color: COLORS.global.black_0,
-  //           fontSize: 14,
-  //           fontFamily: 'Orbitron, sans-serif',
-  //         }
-  //       });
+        toast(`${player?.player.nickname}, your ${inputValue} withdraw was a success`, {
+          autoClose: 5000,
+          pauseOnHover: true,
+          type: 'success',
+          style: {
+            background: COLORS.global.white_0,
+            color: COLORS.global.black_0,
+            fontSize: 14,
+            fontFamily: 'Orbitron, sans-serif',
+          }
+        });
         
-  //       setInputValue('');
-  //     } catch (error: any) {
-  //       const apiErrorResponse = ApiError(error);
+        setInputValue('');
+      } catch (error: any) {
+        const apiErrorResponse = ApiError(error);
 
-  //       apiErrorResponse.messages.map(message => {
-  //         return toast(message, {
-  //           autoClose: 5000,
-  //           pauseOnHover: true,
-  //           type: 'error',
-  //           style: {
-  //             background: COLORS.global.white_0,
-  //             color: COLORS.global.red_0,
-  //             fontSize: 14,
-  //             fontFamily: 'Orbitron, sans-serif',
-  //           }
-  //         });
-  //       });
-  //     } finally {
-  //       withdrawButtonHasBlocked.changeToFalse();
-  //     }
-  //   }
+        apiErrorResponse.messages.map(message => {
+          return toast(message, {
+            autoClose: 5000,
+            pauseOnHover: true,
+            type: 'error',
+            style: {
+              background: COLORS.global.white_0,
+              color: COLORS.global.red_0,
+              fontSize: 14,
+              fontFamily: 'Orbitron, sans-serif',
+            }
+          });
+        });
+      } finally {
+        withdrawButtonHasBlocked.changeToFalse();
+      }
+    }
 
-  // }
+  }
 
-  // async function handleSubmitDeposit(event: React.FormEvent<HTMLButtonElement>) {
-  //   event.preventDefault();
+  async function handleSubmitDeposit(event: React.FormEvent<HTMLButtonElement>) {
+    event.preventDefault();
 
-  //   const verifyHasInputValue = verifyIfHasInputValue(inputValue, 'You need to enter the number to continue the deposit.');
-  //   if(verifyHasInputValue) {
-  //     commonToast();
+    const verifyHasInputValue = verifyIfHasInputValue(inputValue, 'You need to enter the number to continue the deposit.');
+    if(verifyHasInputValue) {
+      commonToast();
 
-  //     depositButtonHasBlocked.changeToTrue();
+      depositButtonHasBlocked.changeToTrue();
 
-  //     try {
-  //       if(player) {
-  //         await verifyWallet(player.player);
-  //       }
+      try {
+        if(player) {
+          await verifyWallet(player.player);
+        }
 
-  //       const { transaction, error } = await paymentByEthereum({
-  //         ethereum: (window as any).ethereum,
-  //         toAddress: ethereumConfig.privateSaleTransaction.toAddress,
-  //         ether: ethers.utils.parseEther(inputValue)._hex,
-  //         dataContract: ethereumConfig.privateSaleTransaction.contract.BNB,
-  //       });
+        const { transaction, error } = await paymentByEthereum({
+          ethereum: (window as any).ethereum,
+          toAddress: ethereumConfig.privateSaleTransaction.toAddress,
+          ether: ethers.utils.parseEther(inputValue)._hex,
+          dataContract: ethereumConfig.privateSaleTransaction.contract.BNB,
+        });
     
-  //       if(error) {
-  //         return toast(error.message, {
-  //           autoClose: 5000,
-  //           pauseOnHover: true,
-  //           type: 'error',
-  //           style: {
-  //             background: COLORS.global.white_0,
-  //             color: COLORS.global.red_0,
-  //             fontSize: 14,
-  //             fontFamily: 'Orbitron, sans-serif',
-  //           }
-  //         });
-  //       }
+        if(error) {
+          return toast(error.message, {
+            autoClose: 5000,
+            pauseOnHover: true,
+            type: 'error',
+            style: {
+              background: COLORS.global.white_0,
+              color: COLORS.global.red_0,
+              fontSize: 14,
+              fontFamily: 'Orbitron, sans-serif',
+            }
+          });
+        }
 
-  //       if(transaction) {
-  //         try {
-  //           toast(`Wait for us to confirm the deposit in our database`, {
-  //             autoClose: 5000,
-  //             pauseOnHover: true,
-  //             type: 'info',
-  //             style: {
-  //               background: COLORS.global.white_0,
-  //               color: COLORS.global.black_0,
-  //               fontSize: 14,
-  //               fontFamily: 'Orbitron, sans-serif',
-  //             }
-  //           });
+        if(transaction) {
+          try {
+            toast(`Wait for us to confirm the deposit in our database`, {
+              autoClose: 5000,
+              pauseOnHover: true,
+              type: 'info',
+              style: {
+                background: COLORS.global.white_0,
+                color: COLORS.global.black_0,
+                fontSize: 14,
+                fontFamily: 'Orbitron, sans-serif',
+              }
+            });
 
-  //           await baseApi.post('/players/deposit-tokens', {
-  //             txHash: transaction
-  //           });
+            await baseApi.post('/players/deposit-tokens', {
+              txHash: transaction
+            });
             
-  //           toast(`${player?.player.nickname}, your ${inputValue} deposit was a success`, {
-  //             autoClose: 5000,
-  //             pauseOnHover: true,
-  //             type: 'success',
-  //             style: {
-  //               background: COLORS.global.white_0,
-  //               color: COLORS.global.black_0,
-  //               fontSize: 14,
-  //               fontFamily: 'Orbitron, sans-serif',
-  //             }
-  //           });
+            toast(`${player?.player.nickname}, your ${inputValue} deposit was a success`, {
+              autoClose: 5000,
+              pauseOnHover: true,
+              type: 'success',
+              style: {
+                background: COLORS.global.white_0,
+                color: COLORS.global.black_0,
+                fontSize: 14,
+                fontFamily: 'Orbitron, sans-serif',
+              }
+            });
             
-  //           setInputValue('');
-  //         } catch (error: any) {
-  //           const apiErrorResponse = ApiError(error);
+            setInputValue('');
+          } catch (error: any) {
+            const apiErrorResponse = ApiError(error);
 
-  //           apiErrorResponse.messages.map(message => {
-  //             return toast(message, {
-  //               autoClose: 5000,
-  //               pauseOnHover: true,
-  //               type: 'error',
-  //               style: {
-  //                 background: COLORS.global.white_0,
-  //                 color: COLORS.global.red_0,
-  //                 fontSize: 14,
-  //                 fontFamily: 'Orbitron, sans-serif',
-  //               }
-  //             });
-  //           });
-  //         }
-  //       }
-  //     } catch(error: any) {
-  //       const apiErrorResponse = ApiError(error);
+            apiErrorResponse.messages.map(message => {
+              return toast(message, {
+                autoClose: 5000,
+                pauseOnHover: true,
+                type: 'error',
+                style: {
+                  background: COLORS.global.white_0,
+                  color: COLORS.global.red_0,
+                  fontSize: 14,
+                  fontFamily: 'Orbitron, sans-serif',
+                }
+              });
+            });
+          }
+        }
+      } catch(error: any) {
+        const apiErrorResponse = ApiError(error);
 
-  //       apiErrorResponse.messages.map(message => {
-  //         return toast(message, {
-  //           autoClose: 5000,
-  //           pauseOnHover: true,
-  //           type: 'error',
-  //           style: {
-  //             background: COLORS.global.white_0,
-  //             color: COLORS.global.red_0,
-  //             fontSize: 14,
-  //             fontFamily: 'Orbitron, sans-serif',
-  //           }
-  //         });
-  //       });
-  //     } finally {
-  //       depositButtonHasBlocked.changeToFalse();
-  //     }
-  //   }
-  // }
+        apiErrorResponse.messages.map(message => {
+          return toast(message, {
+            autoClose: 5000,
+            pauseOnHover: true,
+            type: 'error',
+            style: {
+              background: COLORS.global.white_0,
+              color: COLORS.global.red_0,
+              fontSize: 14,
+              fontFamily: 'Orbitron, sans-serif',
+            }
+          });
+        });
+      } finally {
+        depositButtonHasBlocked.changeToFalse();
+      }
+    }
+  }
 
   return (
     <Container>
@@ -339,35 +344,37 @@ export function AccountTab() {
               </Resource>
             </Resources>
           </ResourcesDetail>
-          {/* <Spc>
-            <InfoTitle_1 className="spc_title">WITHDRAW/DEPOSIT SPC</InfoTitle_1>
-            <input 
-              onChange={(event) => handleChangeInput({
-                event
-              })}
-              value={inputValue}
-              placeholder="Withdraw/desposit spc" 
-              type="text" 
-            />
-            <Button 
-              type="submit" 
-              text="Withdraw"
-              disabled={withdrawButtonHasBlocked.state || depositButtonHasBlocked.state}
-              loading={{
-                state: withdrawButtonHasBlocked.state
-              }}
-              onClick={handleSubmitWithdraw}
-            />
-            <Button 
-              type="submit" 
-              text="Deposit"
-              disabled={withdrawButtonHasBlocked.state || depositButtonHasBlocked.state}
-              loading={{
-                state: depositButtonHasBlocked.state
-              }}
-              onClick={handleSubmitDeposit}
-            />
-          </Spc> */}
+          {withdrawDepositShow && (
+            <Spc>
+              <InfoTitle_1 className="spc_title">WITHDRAW/DEPOSIT SPC</InfoTitle_1>
+              <input 
+                onChange={(event) => handleChangeInput({
+                  event
+                })}
+                value={inputValue}
+                placeholder="Withdraw/desposit spc" 
+                type="text" 
+              />
+              <Button 
+                type="submit" 
+                text="Withdraw"
+                disabled={withdrawButtonHasBlocked.state || depositButtonHasBlocked.state}
+                loading={{
+                  state: withdrawButtonHasBlocked.state
+                }}
+                onClick={handleSubmitWithdraw}
+              />
+              <Button 
+                type="submit" 
+                text="Deposit"
+                disabled={withdrawButtonHasBlocked.state || depositButtonHasBlocked.state}
+                loading={{
+                  state: depositButtonHasBlocked.state
+                }}
+                onClick={handleSubmitDeposit}
+              />
+            </Spc>
+          )}
         </SecondaryDetails>
       </Content>
       <Wallet 
